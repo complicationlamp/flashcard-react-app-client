d098a38110d5f9cda79471cbb5fdb884fa91d4a0
Kyle <lookerkj@protonmail.com>
2018-08-16 16:23:49 -0700

    started on the router with a test file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Nav from './components/nav';
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+        <Nav />
+          <header className="App-header">
+            <Link to="/"><img src={logo} className="App-logo" alt="logo" /></Link>
+            <h1 className="App-title">Learn more</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { Test } from './test';
+
+export default class Nav extends React.Component {
+	render() {
+		return (
+			<nav className="Nav">
+				<div className="Nav-container">
+					<ul className="Nav__item-wrapper">
+                		<li className="Nav__item">
+                 			<Link className="Nav__link" to="/path1">Link 1</Link>
+                		</li>
+                		<li className="Nav__item">
+                  			<Link className="Nav__link" to="/path2">Link 2</Link>
+                		</li>
+                		<li className="Nav__item">
+							<Link className="Nav-link" to="/components/test">Test</Link>
+                		</li>
+              		</ul>
+					  {/* this shold point to the page to render */}
+					  {/* FIX: just poping up below the nav bar */}
+					  <Route path="/components/test" component={Test} />
+				</div>
+			</nav>
+		)
+	}
+}
