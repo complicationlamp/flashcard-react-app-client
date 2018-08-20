7629e8f180168d780ed4db2994a131594dc03417
Kyle <lookerkj@protonmail.com>
2018-08-20 15:58:22 -0700

    added consoleLog to logout

--- src/components/logout.js
+++ src/components/logout.js
@@ -3,8 +3,12 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 export class Logout extends React.Component {
 	render() {
+		function handleClick(e) {
+			e.preventDefault();
+			console.log('Logout was clicked');
+		}
 		return (
-			<Link className="Nav-link" to="/components/home"><button type='submit'>Logout</button></Link>
+			<Link className="Nav-link" to="/components/home"><button type='submit' onClick={handleClick}>Logout</button></Link>
 		)
 	}
 }
