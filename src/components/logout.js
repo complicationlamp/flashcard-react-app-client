cb08c66af7a6b676a602ead8ccb2a589480402ce
Kyle <lookerkj@protonmail.com>
2018-08-19 18:01:16 -0700

    linked logout to the home screed

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 export class Logout extends React.Component {
 	render() {
 		return (
-		<h1>logout</h1>
+			<Link className="Nav-link" to="/components/home"><button type='submit'>Logout</button></Link>
 		)
 	}
 }
