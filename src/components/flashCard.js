0ea378f888937b8844420d5326003037c44fc32e
Kyle <lookerkj@protonmail.com>
2018-08-20 11:13:23 -0700

    added a temp state to flashCard.js

--- src/components/flashCard.js
+++ src/components/flashCard.js
@@ -2,35 +2,45 @@ import React from "react";
 
 export class FlashCard extends React.Component{
 	render() {
+		const cssExampleQuestion={
+			Q:"What does CSS standfor?",
+			ANSWER:"Cascade Styling Sheets",
+			wrongAns1:"Clear Simple Styling",
+			wrongAns2:"Creative Styling Solution",
+			wrongAns3:"Computed Server System",
+			docsLink:"https://developer.mozilla.org/en-US/docs/Web/CSS",
+			}
 		return (
 			<main role="main">
 			<section className="noteCard">
 				<p>=========FRONT OF FLASH========</p>
-				<h1 className="App-quiz-questionHeader"><img src="/media/examples/frog.png" alt="[icon of subject + clickable to docs]" /><strong>Q:</strong>What are the things</h1>
+				<h1 className="App-quiz-questionHeader">
+					<img src="/media/examples/frog.png" alt="[icon of subject + clickable to docs]" />
+					<strong>Q:</strong>{cssExampleQuestion.Q}</h1>
 				<div class="custom-control custom-radio">
 					<input type="radio" id="customRadio1" name="customRadio" class="custom-control-input"/>
-					<label class="custom-control-label" for="customRadio1">Answere 1</label>
+					<label class="custom-control-label" for="customRadio1">{cssExampleQuestion.wrongAns1}</label>
 				</div>
 				<div class="custom-control custom-radio">
 					<input type="radio" id="customRadio2" name="customRadio" class="custom-control-input"/>
-					<label class="custom-control-label" for="customRadio2">Answer 2</label>
+					<label class="custom-control-label" for="customRadio2">{cssExampleQuestion.ANSWER}</label>
 				</div>
 				<div class="custom-control custom-radio">
 					<input type="radio" id="customRadio1" name="customRadio" class="custom-control-input"/>
-					<label class="custom-control-label" for="customRadio1">Answere 3</label>
+					<label class="custom-control-label" for="customRadio1">{cssExampleQuestion.wrongAns2}</label>
 				</div>
 				<div class="custom-control custom-radio">
 					<input type="radio" id="customRadio2" name="customRadio" class="custom-control-input"/>
-					<label class="custom-control-label" for="customRadio2">Answer 4</label>
+					<label class="custom-control-label" for="customRadio2">{cssExampleQuestion.wrongAns3}</label>
 				</div>
 				<button className="App-flashcard-flip">Flip Card</button>
 			</section>
 			<section>
 				<div className="noteCard-back">
 					<p>=========Back of NoteCard=========</p>
-					<h1 className="noteCard-header1">Question #111</h1>
-					<p><strong>ANSWER:</strong> answer answer</p>
-					<a href='#'>this is the link to the docs</a>
+					<h1 className="noteCard-header1">{cssExampleQuestion.Q}</h1>
+					<p><strong>ANSWER:</strong> {cssExampleQuestion.ANSWER}</p>
+					<a href={cssExampleQuestion.docsLink}>this is the link to the docs</a>
 			<button className="App-flashcard-prev">Previous Card</button>
 			<button className="App-flashcard-next">Next Card</button>
 				</div>
