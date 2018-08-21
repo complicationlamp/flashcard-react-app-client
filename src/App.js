08b78960f4b892f7c8ecad7f5d543a114c024840
Kyle <lookerkj@protonmail.com>
2018-08-21 14:22:01 -0700

    CHANGED: added a redirect path to the the app.js to render the home page on initial load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Nav from './components/nav';
 import { Home } from './components/home';
 import { Login } from './components/login';
@@ -21,6 +21,9 @@ class App extends Component {
       <Router>
         <div className="App">
           <Nav />
+          <Route exact path="/" render={() => (
+            <Redirect to="/home"/>
+          )}/>
           {/* //===================== buildnote====================================
           // TODO: ADD ROUTS
           // =====================================================================// */}
