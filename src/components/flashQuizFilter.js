162f406b299bb89163be274f0c137158ce007005
Kyle <lookerkj@protonmail.com>
2018-08-27 14:29:27 -0700

    adding some empty state stuff

--- src/components/flashQuizFilter.js
+++ src/components/flashQuizFilter.js
@@ -2,6 +2,19 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export class Filter extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			subject: {},
+			question: {},
+			answer: {},
+			wrongAnsOne: {},
+			wrongAnsTwo: {},
+			wrongAnsThree: {}, 
+			link: {}
+		};
+	}
 	render() {
 		return (
 			<section>
