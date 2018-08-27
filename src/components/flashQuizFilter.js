b59547f550bf8bf992bacb6d424feab0ccee0437
Kyle <lookerkj@protonmail.com>
2018-08-27 14:30:29 -0700

    put them in the wrong file

--- src/components/flashQuizFilter.js
+++ src/components/flashQuizFilter.js
@@ -2,19 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export class Filter extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			subject: {},
-			question: {},
-			answer: {},
-			wrongAnsOne: {},
-			wrongAnsTwo: {},
-			wrongAnsThree: {}, 
-			link: {}
-		};
-	}
 	render() {
 		return (
 			<section>
