bd6be49fbc244e04f8d4deb6e892fbe3ae9395d5
Kyle <lookerkj@protonmail.com>
2018-09-14 14:58:41 -0700

    gramer error

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import {Login} from '../components/Login';
+import {Login} from '../components/login';
 
 describe('<Login>', () => {
 	it('Renders without crashing', () => {
