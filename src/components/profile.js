f53a5adeb06f732e40f7002e08f3b2f895750775
Kyle <lookerkj@protonmail.com>
2018-09-14 13:41:44 -0700

    moved makeflashcard to the other side of profile

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,24 +1,27 @@
 import React from "react";
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import {Feedback} from './feedback'
+// import {Feedback} from './feedback'
+import {MakeFlashcard} from './makeFlashcard'
 
 import './cssComponents/profile.css'
 export class Profile extends React.Component {
 	render() {
 		return (
 				<main role="main">
-					<section className="App-profile-welcome">
+					<section className="App-profile-welcome col-12">
 						<header role="banner">
 							<h1>Welcome {this.props.userFirstName}</h1>
-							<h2>Here's how you're doing so far</h2>
-							<Feedback/>
+							{/* <h2>Here's how you're doing so far</h2> */}
 						</header>
 					</section>
-					<section className="links-section col-12">
+					<section className="links-section col-6">
 						<Link className="linkFromProfile row" to="/flashQuiz">Start a session</Link>
 						<Link className="linkFromProfile row" to="/deleteProfile">Delete Profile</Link>
 						<Link className="linkFromProfile row" to="/makeFlashcard">Make Flashcard</Link>
+					</section>
+					<section className="makeFC-profile col-6">
+						<MakeFlashcard/>
 					</section>
 				</main>
 		)
